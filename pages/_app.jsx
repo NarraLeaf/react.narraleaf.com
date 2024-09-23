84d449101ddcdc60fe012c86679e16c2f2b591ab
helloyork <helloyork@icloud.com>
2024-09-22 21:15:03 -0700

    add vercel support

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import "../src/base.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return <><Analytics /><Component {...pageProps} /></>
 }
