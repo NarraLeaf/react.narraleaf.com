3419b3d20f95701e224342fbc2e1fc8cf58ff4a6
Nomen <111544765+helloyork@users.noreply.github.com>
2026-02-19 13:09:28 -0800

    update: context7

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,18 @@
 import "../src/base.css";
 import { Analytics } from "@vercel/analytics/react";
 import HighlightSpan from "../src/components/highlight-span";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-    return <><HighlightSpan /><Analytics /><Component {...pageProps} /></>
+    return (
+        <><HighlightSpan />
+            <Analytics />
+            <Component {...pageProps} />
+            <Script
+                src="https://context7.com/widget.js"
+                data-library="/narraleaf/react.narraleaf.com"
+                strategy="afterInteractive"
+            />
+        </>
+    );
 }
